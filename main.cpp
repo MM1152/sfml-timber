a277f439404fe1a23c81cdb3f4123038a7fc3998
천민성 <albert6306@naver.com>
2025-06-10 17:21:08 +0900

    SFML 배경이미지 뛰우기

--- main.cpp
+++ main.cpp
@@ -1,27 +1,31 @@
 #include <SFML/Graphics.hpp>
 
-using namespace sf;
+
+
+
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(300, 300), "abs"); // 화면에 뛰울 윈도우의 사이즈 / 제목
-    sf::CircleShape shape(50.f);  // 화면에 그릴 원의 반지름
-    shape.setFillColor(sf::Color::Green); // 그림을 그린 shape의 색깔
+    sf::RenderWindow window(sf::VideoMode(1920 , 1080), "Timber!"); // 화면에 뛰울 윈도우의 사이즈 / 제목
+
+    sf::Texture textureBackGround; // background Image 로딩용
+    textureBackGround.loadFromFile("Resources/graphics/background.png");
+
+    sf::Sprite spriteBackGround;
+    spriteBackGround.setTexture(textureBackGround);
 
     while (window.isOpen())
     {
         sf::Event event;
+
 
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        
-        
-        
         window.clear(); 
-        window.draw(shape);
+        window.draw(spriteBackGround);
         window.display();
     }
 
